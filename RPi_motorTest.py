0030f06d9f8270c1e4e6bd4206f02314bc1966a3
be7z <40293181+be7z@users.noreply.github.com>
2019-04-05 16:37:09 +0700

    Multi-Motors Test

--- RPi_motorTest.py
+++ RPi_motorTest.py
@@ -2,28 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-'''
-servoPIN = 4
-GPIO.setup(servoPIN, GPIO.OUT)
-pwmL = GPIO.PWM(servoPIN, 50)
-pwmL.start(0)
 
-servoPIN = 18
-GPIO.setup(servoPIN, GPIO.OUT)
-pwmM = GPIO.PWM(servoPIN, 50)
-pwmM.start(0)
-
-servoPIN = 16
-GPIO.setup(servoPIN, GPIO.OUT)
-pwmRB = GPIO.PWM(servoPIN, 50)
-pwmRB.start(0)
-
-servoPIN = 21
-GPIO.setup(servoPIN, GPIO.OUT)
-pwmLB = GPIO.PWM(servoPIN, 50)
-pwmLB.start(0)
-
-'''
 servoPIN = 21
 GPIO.setup(servoPIN, GPIO.OUT)
 pwmGripper = GPIO.PWM(servoPIN, 50)
@@ -36,8 +15,8 @@ pwmT.start(0)
 
 
 while True:
-    #maxGripper 2.5 --> min for bottle 9.5
-    #maxT 17 --> 
+    # maxGripper 2.5 --> min for bottle 9.5
+    # maxTopJoint 17 for 100PMW
     pwmT.ChangeDutyCycle(9)
     time.sleep(0.75)
     pwmGripper.ChangeDutyCycle(2.5)
@@ -46,4 +25,3 @@ while True:
     time.sleep(0.75)
     pwmGripper.ChangeDutyCycle(10)
     time.sleep(0.75)
-
